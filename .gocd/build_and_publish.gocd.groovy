79998a54185513bb6fd3316059c432518dc281ee
Bhupendrakumar Piprava <bdpiparva@gmail.com>
2019-07-10 16:47:19 +0530

    Making publish stage manual

--- .gocd/build_and_publish.gocd.groovy
+++ .gocd/build_and_publish.gocd.groovy
@@ -40,28 +40,38 @@ GoCD.script {
           artifactCleanupProhibited = false
           cleanWorkingDir = true
           fetchMaterials = true
+          approval {
+            type = 'manual'
+          }
           environmentVariables = [
-            GNUPGHOME            : '~/.code-signing-keys',
+            GNUPGHOME            : '.signing',
             GOCD_GPG_KEYRING_FILE: 'signing-key.gpg',
           ]
           secureEnvironmentVariables = [
             GOCD_NEXUS_USERNAME: 'AES:H0l96KkJWnHkWdJPVdha2Q==:XvKyOuun43zh4L+TSpbGvw==',
             GOCD_NEXUS_PASSWORD: 'AES:r/8x3K1wEReuRZRmm37h/g==:ASQOrYbpwigCPA8GfkQi7NWDzejOTean0v9XwYUcvjPlgWdvR6xnvbwvrAdBE6Rg',
             GOCD_GPG_PASSPHRASE: 'AES:7lAutKoRKMuSnh3Sbg9DeQ==:8fhND9w/8AWw6dJhmWpTcCdKSsEcOzriQNiKFZD6XtN+sJvZ65NH/QFXRNiy192+SSTKsbhOrFmw+kAKt5+MH1Erd6H54zJjpSgvJUmsJaQ=',
-            GOCD_GPG_KEY_ID    : 'AES:abQlKbCl6aE5NgVisyu7jg==:/wrZ34d5qiawh14DxK/6JQbw22kPO+k7HNcuFqJOqL8='
+            GOCD_GPG_KEY_ID    : 'AES:+ORNmqROtoiLtfp+q4FlfQ==:PxQcI6mOtG4J/WQHS9jakg=='
           ]
           jobs {
             job('upload-to-maven') {
               elasticProfileId = 'ecs-gocd-dev-build'
               tasks {
                 bash {
-                  commandString = 'echo $GOCD_GPG_PASSPHRASE > gpg-passphrase'
+                  commandString = 'mkdir -p ${GNUPGHOME}'
+                  workingDir = "plugin-base"
+                }
+                bash {
+                  commandString = 'echo ${GOCD_GPG_PASSPHRASE} > gpg-passphrase'
+                  workingDir = "plugin-base"
                 }
                 bash {
                   commandString = 'gpg --quiet --batch --passphrase-file gpg-passphrase --output - ../signing-keys/gpg-keys.pem.gpg | gpg --import --batch --quiet'
+                  workingDir = "plugin-base"
                 }
                 bash {
-                  commandString = 'gpg --export-secret-keys $GOCD_GPG_KEY_ID > $GOCD_GPG_KEYRING_FILE'
+                  commandString = 'gpg --export-secret-keys ${GOCD_GPG_KEY_ID} > ${GOCD_GPG_KEYRING_FILE}'
+                  workingDir = "plugin-base"
                 }
                 bash {
                   commandString = './gradlew clean uploadArchives closeAndReleaseRepository'
